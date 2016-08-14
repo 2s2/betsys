6d41d2f12ea0c103c8b219db7c7e467af27beb7d
2s2 <2flap2@gmail.com>
2016-08-15 01:38:09 +0300

    Initial - archive.php

---
+++ www/betsystem.cc/wp-content/themes/betsystem/archive.php
@@ -0,0 +1,37 @@
+<?php   
+get_header(); 
+?> 
+   
+    <div class="b-center-content">
+        <? if(have_posts()): while(have_posts()):the_post();?> 
+
+          <div class="b-slot-item">
+            <?php the_post_thumbnail('slot-thumb'); ?> 
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+          </div>    
+
+           <?php endwhile; endif;  ?> 
+
+        <div class="nav_div">
+              <div class="nav">
+              <?php if(function_exists('wp_pagenavi')): ?>
+              <?php wp_pagenavi(); ?><?php else : ?><?php posts_nav_link(); ?>
+              <?php endif; ?>
+              </div> 
+          </div>  
+
+        </div>
+
+
+
+<?php get_sidebar('single'); ?>
+
+
+     </div> 
+
+<?php include(TEMPLATEPATH.'/include/priem.php'); ?>
+
+
+<?php get_footer(); ?>
+
+  
