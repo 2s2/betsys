f670faa063cc1508a8116e59f246ebe15e6a8d30
Vyacheslav Lyovkin <v.lyovkin@astoundcommerce.com>
2016-09-26 23:04:34 +0300

    Initial - sidebar.php

---
+++ www/betsystem.cc/wp-content/themes/betsystem/sidebar.php
@@ -0,0 +1,21 @@
+	<div class="b-sidebar"> 
+ 					
+ 					<div class="b-side-menu">
+ 						  <?php if(ICL_LANGUAGE_CODE=='ru'): ?>
+
+			                 <?php wp_nav_menu('menu=left_menu_ru'); ?>   
+
+			              <?php elseif(ICL_LANGUAGE_CODE=='en'): ?>
+
+			                 <?php wp_nav_menu('menu=left_menu_en'); ?>  
+
+			              <?php elseif(ICL_LANGUAGE_CODE=='es'): ?>
+
+			                <?php wp_nav_menu('menu=left_menu_es'); ?>  
+
+			              <?php endif;?> 
+ 
+ 					</div>
+					
+<a href="/payments-from-home/"><img src="http://betsystem.cc/wp-content/uploads/2015/09/1xbet.png" alt="Payments from home" style="border:1px solid #999;" /></a>
+				</div>
